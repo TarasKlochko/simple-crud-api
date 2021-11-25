17613bf025b7140434a9f465287114cc0a2aafbb
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-11-25 18:47:50 +0200

    feat: implement uuid validation/response 400 for get request

--- controllers/personController.js
+++ controllers/personController.js
@@ -1,5 +1,7 @@
 const Persons = require('../models/personeModel');
 const { getPostData } = require('../utils');
+const { version: uuidVersion } = require('uuid');
+const { validate: uuidValidate } = require('uuid');
 
 async function getPersons(req, res) {
   try {
@@ -13,13 +15,19 @@ async function getPersons(req, res) {
 
 async function getPerson(req, res, id) {
   try {
-    const person = await Persons.findById(id);
-    if (person) {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(person));
+    const isUuidV4Valid = uuidValidate(id) && uuidVersion(id) === 4;
+    if (isUuidV4Valid) {
+      const person = await Persons.findById(id);
+      if (person) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(person));
+      } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Person not found' }));
+      }
     } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'Person not found' }));
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: ' This code is not valid ' }));
     }
   } catch (error) {
     console.log(error);
